2f4af50335ea06296663333f41f0fdd412991de9
Allan IRDEL <allan.irdel@gmail.com>
2017-03-25 17:42:54 +0100

    update enable code

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # DWP-VPS-MUTU-01
+
+cd /etc/puppet/environments/webhosts
+git init
+git remote add origin git@github.com:Ddall/DWP-VPS-MUTU-01.git
+git fetch
+git reset --hard origin/master
+bash /etc/puppet/environments/webhosts/scripts/puppet-webhosts-apply.sh
--- scripts/puppet-webhosts-apply.sh
+++ scripts/puppet-webhosts-apply.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /etc/puppet/
+cd /etc/puppet/environments/webhosts
 git pull origin master
 
 puppet apply /etc/puppet/environments/webhosts/manifests/site.pp --environment=webhosts --modulepath=/etc/puppet/modules:/etc/puppet/environments/webhosts/modules:/etc/puppet/environments/main/modules
